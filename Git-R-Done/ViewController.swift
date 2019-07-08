a2c92f5ddae5d467a69ee8bc8345375e9efd085d
Alex Zhou <zhoua2021@gmail.com>
2019-07-08 09:43:34 -0500

    ejfiosla

---
+++ Git-R-Done/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Git-R-Done
+//
+//  Created by Zhou, Alexander on 7/8/19.
+//  Copyright © 2019 Alex Zhou. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let variable = ""
+        print(variable)
+    }
+
+
+}
+
